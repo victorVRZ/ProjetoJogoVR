bae0e3fb4bebe92678c35e008a8e94fd38506b8c
victorVRZ <vrviebrantz@gmail.com>
2026-05-09 10:10:08 -0300

    restart scene script

---
+++ Assets/Scripts/RestartScene.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+
+public class RestartScene : MonoBehaviour
+{
+    public InputDevice leftControler;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
